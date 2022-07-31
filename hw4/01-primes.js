98db74676b5897de7781c1dbe275ac3811a4afbd
archie <archithabasavaraju@gmail.com>
2022-07-31 13:37:31 -0700

    primes using factors

--- hw4/01-primes.js
+++ hw4/01-primes.js
@@ -1,19 +1,19 @@
 const getPrimes = () => {
   // Add your code here
+  console.log("Prime numbers between 1 to 100:");
+  factor = 0;
+  for (n = 2; n <= 100; n++) {
+    for (i = 1; i <= n; i++) {
+      if (n % i == 0) {
+        factor++;
+      }
+    }
+    if (factor = 2) {
+      console.log(n);
+    }
+    factor = 0;
+  }
 };
 
 getPrimes();
-// 2
-// 3
-// 5
-// 7
-// 11
-// 13
-// 19
-// 23
-// 29
-// 31
-// 37
-// 41
-// 43
-// ...
+
