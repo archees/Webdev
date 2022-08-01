870ab0b432be43d3a56b2a2bab9104d57443ecea
archie <archithabasavaraju@gmail.com>
2022-07-31 20:43:32 -0700

    hw

--- hw4/04-hello.js
+++ hw4/04-hello.js
@@ -1,1 +1,25 @@
 // Add your code here
+const main = document.querySelector("main");
+let bold = document.createElement("strong");
+const paragraph = document.createElement("p");
+
+const image = document.createElement("img");
+image.src = "http://placekitten.com/200/200";
+image.alt = "cat";
+image.style.height = "200px";
+image.style.width = "200px";
+image.style.borderRadius = "50%";
+
+let node = document.createTextNode(
+    "Hey! This is Archita Basavaraju."
+  );
+let text = "I am currently pursuing my graduate studies at PSU. I look forward to learning fun stuff! My hobbies are reading fantasy novel, watching thriller movies, playing badminton and having fun conversations. Oh I love hanging out with my friends and eating good food!";
+
+main.appendChild(image);
+bold.appendChild(node);
+paragraph.appendChild(bold);
+paragraph.appendChild(document.createElement("br"));
+paragraph.append(text);
+paragraph.appendChild(document.createElement("br"));
+main.appendChild(paragraph);
+main.setAttribute("style", "text-align:center");
