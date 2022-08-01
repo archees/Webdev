aa780df41e50ca03d810dfd6f6926c8e858cb249
archie <archithabasavaraju@gmail.com>
2022-07-31 17:30:25 -0700

    timelapsed

--- hw4/03-time.js
+++ hw4/03-time.js
@@ -1,10 +1,33 @@
 const calculateTime = (date1, date2) => {
   // Given two dates, calculate and return the amount of time elapsed in years and months
+  let output = "";
+  let num = new Date(date1);
+  if (isNaN(num)) {
+    return output.concat("Invalid Input");
+  }
+  let inputdate = new Date(date2);
+  if (isNaN(inputdate)) {
+    return output.concat("Invalid Input");
+  }
+  let years = num.getFullYear() - inputdate.getFullYear();
+  let months = num.getMonth() - inputdate.getMonth();
+
+  if (years > 0) {
+    if (months > 1) {
+      return output.concat("TimeLapsed:  ", years, " years, ", months, " months ");
+    } else if (months == 1) {
+      return output.concat("TimeLapsed: ", years, " years, ", months, " month ");
+    } else if (months < 1) {
+      return output.concat("TimeLapsed: ", years, " years, ");
+    }
+  } else {
+    return output.concat("TimeLapsed: ", months, " months ");
+  }
 };
 
 // Date() formats:
 // new Date('December 1, 1995')
-// new Date('2008-1-11')
+// new Date('2008-1-11') 
 // new Date(2020, 3, 16)
 // new Date(628021800000)
 
